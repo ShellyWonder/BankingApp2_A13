2e05cd8855fa31ef03124309699835cad96a4482
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-02-16 12:07:03 -0600

    file changes to enable css file

--- src/main/java/com/coderscampus/assignment13/WebMvcConfig.java
+++ src/main/java/com/coderscampus/assignment13/WebMvcConfig.java
@@ -9,10 +9,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
   @Override
-  public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    registry.addResourceHandler("/resources/**")
-       .addResourceLocations("/resources/static/"); 
+public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
-  }
+  registry.addResourceHandler("/resources/static/**") 
+    .addResourceLocations("/resources/static/");
+
+} 
+
 
 }
