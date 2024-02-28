02c07d9eff52aeec66fbbdab855b7a2964e08d6f
Shelly Wonder <ShellyJ.Wonder@outlook.com>
2024-02-28 16:53:09 -0600

    Complete update address functionality

--- src/main/java/com/coderscampus/assignment13/service/UserService.java
+++ src/main/java/com/coderscampus/assignment13/service/UserService.java
@@ -16,6 +16,8 @@ import com.coderscampus.assignment13.domain.User;
 import com.coderscampus.assignment13.repository.AccountRepository;
 import com.coderscampus.assignment13.repository.UserRepository;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -60,14 +62,46 @@ public class UserService {
 	}
 
 	@Transactional
-	public User updateUserAndAddress(User updatedUser) {
-		// Ensure the user is set on the address if it exists
-		if (updatedUser.getAddress() != null) {
-			updatedUser.getAddress().setUser(updatedUser);
-		}
-		// Because of CascadeType.ALL, saving the user will automatically save address changes as well
-		return userRepo.save(updatedUser);
-	}
+public User updateUserAndAddress(User updatedUser) {
+    // Check if the user already exists in the database
+    Optional<User> existingUserOpt = userRepo.findById(updatedUser.getUserId());
+    if (existingUserOpt.isPresent()) {
+        User existingUser = existingUserOpt.get();
+
+        // Update fields on the existingUser with values from updatedUser
+        existingUser.setUsername(updatedUser.getUsername());
+        existingUser.setPassword(updatedUser.getPassword());
+        existingUser.setName(updatedUser.getName());
+        // ...update other fields as necessary
+
+        // Check and update address
+        Address updatedAddress = updatedUser.getAddress();
+        if (updatedAddress != null) {
+            // update existing address
+            if (existingUser.getAddress() != null) {
+                Address existingAddress = existingUser.getAddress();
+                existingAddress.setAddressLine1(updatedAddress.getAddressLine1());
+                existingAddress.setAddressLine2(updatedAddress.getAddressLine2());
+                existingAddress.setCity(updatedAddress.getCity());
+                existingAddress.setRegion(updatedAddress.getRegion());
+                existingAddress.setCountry(updatedAddress.getCountry());
+                existingAddress.setZipCode(updatedAddress.getZipCode());
+                // Since the existing address is already associated with the user, no need to set the user again
+            } else {
+                // If the existing user does not have an address, associate the new address with the user
+                updatedAddress.setUser(existingUser);
+                existingUser.setAddress(updatedAddress);
+            }
+        }
+
+        // Save the updated existingUser back to the database
+        return userRepo.save(existingUser);
+    } else {
+        // Handle case where the user does not exist, perhaps throw an exception
+        throw new EntityNotFoundException("User with ID " + updatedUser.getUserId() + " not found.");
+    }
+}
+
 
 	public User saveUser(User user) {
 		if (user.getUserId() == null) {
